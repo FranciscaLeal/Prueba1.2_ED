88dc3f30d676a0d34d96e76e30a70012f7351da7
FranciscaLeal <66851504+FranciscaLeal@users.noreply.github.com>
2020-06-15 21:11:02 -0400

    Update and rename .gitignore to prueba1.c

---
+++ prueba1.c
@@ -0,0 +1,13 @@
+float promedio(Lista *lista){
+  int x, largo_lista, suma;
+  suma = 0;
+  //utilizando las funciones dentro del TDAlista
+  //la función Fin entrega la posición del último valor(n-1)
+  largo_lista = Fin(lista); 
+  
+  for (x = 0; x < largo_lista; x++){
+    suma+ = Recuperar(i, L);
+    //se utiliza la funcion recuperar para que vaya sumando cada elemento de la lista
+  }
+  return (suma/largo_lista);
+}
